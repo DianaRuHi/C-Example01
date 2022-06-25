2a4847efce4d653517cd931a94539193b2ace1aa
Diana <diana@example.com>
2022-06-25 17:14:43 +0300

    add Example10 MethodArray

---
+++ Example10MethodArray/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] array = { 145, 62, 23, 54, 95, 216, 78, 38, 49, 47, 8, 0, 345 };
+
+int n = array.Length;
+
+Console.WriteLine("Введите число.");
+Console.Write("num = ");
+int num = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+int found = -1;
+while (index < n)
+{
+    if (num == array[index])
+    {
+        found = index;
+        break;
+    }
+    index++;
+}
+if (found == -1) Console.WriteLine("В массиве нет такого числа");
+else Console.WriteLine($"Позиция числа в массиве {found + 1}");
