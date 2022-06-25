3a2ae71c1177dbec49ef059ad6ebd41f603f5ee2
Diana <diana@example.com>
2022-06-25 18:37:36 +0300

    add Example11 ArrayLibrary

---
+++ Example11_ArrayLibrary/Program.cs
@@ -0,0 +1,41 @@
+﻿void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count - 1)
+    {
+        Console.Write($"{col[position]}, ");
+        position++;
+    }
+    Console.WriteLine($"{col[position]}");
+}
+int IndexOff(int[] collec, int find)
+{
+    int lenght = collec.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        if (find == collec[index])
+        {
+            return index;
+        }
+        index++;
+    }
+    return -1;
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+int find = 4;
+Console.WriteLine(IndexOff(array, find));
