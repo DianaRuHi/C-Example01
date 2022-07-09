ff663e481e593f13b5db98b79d1b0fb89345ab72
Diana <diana@example.com>
2022-07-09 22:54:52 +0300

    Add Example17 Fibonachi Cerursion

---
+++ Example17_FibonachiCerursion/Program.cs
@@ -0,0 +1,10 @@
+﻿int FibonachiNumber(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return FibonachiNumber(n - 1) + FibonachiNumber(n - 2);
+}
+
+for (int i = 1; i < 20; i++)
+{
+    Console.WriteLine($"F{i} = {FibonachiNumber(i)}");
+}
