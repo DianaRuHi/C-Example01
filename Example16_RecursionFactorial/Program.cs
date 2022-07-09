9ef00bd7ca393036a91f12346f62c0de95e670d0
Diana <diana@example.com>
2022-07-09 22:54:14 +0300

    Add Example16 Recursion Factorial

---
+++ Example16_RecursionFactorial/Program.cs
@@ -0,0 +1,10 @@
+﻿int FactorialNumber(int n)
+{
+    if (n == 1) return 1;
+    else return n * FactorialNumber(n - 1);
+}
+
+for (int i = 1; i < 20; i++)
+{
+    Console.WriteLine($"{i}! = {FactorialNumber(i)}");
+}
